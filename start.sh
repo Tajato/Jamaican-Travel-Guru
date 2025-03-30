75361ee80ca822d97ade87817588ac05ce80c058
Tahj Gordon <tahjalex1@gmail.com>
2025-03-30 18:22:56 -0400

    added code to reduce memory usgae on render free tier

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 # This file tells Render.com how to start my app in production
 
-
-uvicorn backend.main:app --host 0.0.0.0 --port $PORT
+uvicorn backend.main:app --host 0.0.0.0 --port $PORT --workers 1 --no-access-log
