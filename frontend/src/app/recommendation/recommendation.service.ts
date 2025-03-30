58cd40ad26e439c2563f6dccdaeeb49a0107c115
Tahj Gordon <tahjalex1@gmail.com>
2025-03-30 19:40:04 -0400

    edited CORS and added retries

--- frontend/src/app/recommendation/recommendation.service.ts
+++ frontend/src/app/recommendation/recommendation.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.prod';
+import { delay, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,6 @@ export class RecService {
 
   // calls the fastAPI backend
   getRecommendations(query: string) {
-    return this.http.post(`${this.apiUrl}/recommend`, { text: query });
+    return this.http.post(`${this.apiUrl}/recommend`, { text: query }).pipe(retry(3), delay(1000));
   }
 }
